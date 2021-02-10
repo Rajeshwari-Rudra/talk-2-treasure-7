65c770adcb201799be055a0486d6b073ae77f282
GD-Prasad <g.d.prasad004@gmail.com>
2021-02-10 09:12:17 -0600

    made css changes .

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-function myFunction() {
+function colorFunction1() {
     const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", ];
 
     const random = Math.floor(Math.random() * values.length);
 
     let value = values[random];
-    document.getElementById("bgrone").style.backgroundColor = "#000000";
+    document.getElementById("bgrone").style.backgroundColor = "#7aeb7a";
 
 
     document.getElementById("lbl").innerHTML = value;
-    let utterance = new SpeechSynthesisUtterance(`     You have picked the card of value     ${value}`);
+    let utterance = new SpeechSynthesisUtterance(`     You have picked the card of color Light Green of thevalue     ${value}`);
     speechSynthesis.speak(utterance);
 
 }
 
-function myFunction2() {
+function colorFunction2() {
 
 
 
@@ -22,10 +22,11 @@ function myFunction2() {
     const random = Math.floor(Math.random() * values.length);
 
     let value = values[random];
-    document.getElementById("bgrtwo").style.backgroundColor = "#ffffff";
+
+    document.getElementById("bgrtwo").style.backgroundColor = "#99cfe0";
 
     document.getElementById("lbl2").innerHTML = value;
-    let utterance = new SpeechSynthesisUtterance(`     You have picked the card of value     ${value}`);
+    let utterance = new SpeechSynthesisUtterance(`     You have picked the card of color Light Blue of the value     ${value}`);
     speechSynthesis.speak(utterance);
 
 }

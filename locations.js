3d3ffca93bead3a28fe50db0658e4e428149258b
Rudravaram <S538361@nwmissouri.edu>
2021-02-17 10:42:11 -0600

    update file name

--- locations.js
+++ locations.js
@@ -1,29 +1,29 @@
 export default [
-//     {
-//       Name: 'Mesa',
-//        Latitude: 33.4271871,
-//        Longitude: -111.8848516
-//   },
-//   {
-//       Name: 'Villago',
-//       Latitude: 33.423360,
-//       Longitude: -111.889613
-//   },
-//   {
-//     Name: 'Horizon Apartments',
-//     Latitude: 40.3589695,
-//     Longitude: -94.8831951
-// },
-// {
-//     Name: 'Colden Pond',
-//     Latitude: 40.3500419,
-//     Longitude: -94.8851095
-// },
-// {
-//     Name: 'B. D. Owens Library',
-//      Latitude: 40.3535922,
-//      Longitude:-94.888208
-// },
+   {
+       Name: 'Mesa',
+       Latitude: 33.4271871,
+      Longitude: -111.8848516
+  },
+  {
+    Name: 'Villago',
+   Latitude: 33.423360,
+     Longitude: -111.889613
+ },
+ {
+    Name: 'Horizon Apartments',
+    Latitude: 40.3589695,
+    Longitude: -94.8831951
+},
+ {
+    Name: 'Colden Pond',
+    Latitude: 40.3500419,
+     Longitude: -94.8851095
+},
+{
+         Name: 'B. D. Owens Library',
+   Latitude: 40.3535922,
+     Longitude:-94.888208
+ },
 {
      Name: 'Valk Center',
       Latitude: 40.352821097563805,

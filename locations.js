9c957a35b59ecf8a7a71e8f85eb45726fea8992a
KHARIKA17 <60010885+KHARIKA17@users.noreply.github.com>
2021-02-19 08:46:16 -0600

    made changes

--- locations.js
+++ locations.js
@@ -1,37 +1,21 @@
 export default [
    {
-       Name: 'Mesa',
-       Latitude: 33.4271871,
-      Longitude: -111.8848516
-  },
-  {
-    Name: 'Villago',
-   Latitude: 33.423360,
-     Longitude: -111.889613
- },
- {
-    Name: 'Horizon Apartments',
-    Latitude: 40.3589695,
-    Longitude: -94.8831951
-},
- {
-    Name: 'Colden Pond',
-    Latitude: 40.3500419,
-     Longitude: -94.8851095
-},
-{
-         Name: 'B. D. Owens Library',
-   Latitude: 40.3535922,
-     Longitude:-94.888208
- },
-{
-     Name: 'Valk Center',
-      Latitude: 40.352821097563805,
-      Longitude:-94.88050461598701
- } ,
-{
-          Name: 'Villago',
-          Latitude: 33.423360,
-           Longitude: -111.889613
-  }
-  ];
+       Name: 'Villago',
+       type:'triangle',
+       coordinates: 
+       [
+         {
+       latitude: 33.4271871,
+       longitude: -111.8848516
+       },
+      {
+     latitude: 33.4271852,
+     longitude: -111.8848570
+     },
+     {
+    latitude: 33.4271822,
+    longitude: -111.8848589
+    }
+]
+}
+];
